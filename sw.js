b214416ca7e20b10f7a367b83c4fedd0deb7d375
Tomer Nahum <github@tomer.nahum.org>
2023-02-16 16:22:24 -0500

    implemented icons including pwa stuff

--- sw.js
+++ sw.js
@@ -3,11 +3,14 @@
 
 
 self.addEventListener("install", event => {
-    console.log("service worker installed")
+    console.log("service worker installed", event)
 });
 
 
 self.addEventListener("activate", event => {
-    console.log("service worker activated")
+    console.log("service worker activated", event)
 })
 
+self.addEventListener("fetch", event => {
+    console.log("fetch event", event)
+})
