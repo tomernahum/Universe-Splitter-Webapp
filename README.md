8af0d4ffe3b5d8c2a12255422c6d13a1c8c72307
Tomer Nahum <github@tomer.nahum.org>
2023-02-16 21:34:16 -0500

    I believe I have gotten offline mode to work and it to be a pwa

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Universe-Splitter-Webapp
 Vanilla HTML+CSS+JS Site to call an API to split the universe (according to one theory), inspired by http://cheapuniverses.com/universesplitter/ iphone app
 
-- Working on more features
-- Working on making it a PWA ASAP
+- Working on more features (see todo.txt)
--- sw.js
+++ sw.js
@@ -1,16 +1,50 @@
 //TODO: cache files for offline use
 
+//ts-check
+
+const CACHE_NAME = "cache-v1";
+const RESOURCES_TO_PRECACHE = [
+    "/",
+    "/index.html",
+    "/script.js",
+    "/style.css",
+    "/explanation.html",
+    "/Spinner-1s-200px.gif",
+    "/manifest.json",
+     
+    //I think favicons etc dont need to be included but im not sure
+    "/images/icons/favicon.ico",
+    "/images/icons/favicon-16x16.png",
+    "/images/icons/favicon-32x32.png",
+
+];
 
 
-self.addEventListener("install", event => {
-    console.log("service worker installed", event)
+
+self.addEventListener("install", e => {
+    console.log("service worker installed", e)
+
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(chache => {
+            return chache.addAll(RESOURCES_TO_PRECACHE)
+        })
+    )
 });
 
 
-self.addEventListener("activate", event => {
-    console.log("service worker activated", event)
+self.addEventListener("activate", e => {
+    console.log("service worker activated", e)
 })
 
-self.addEventListener("fetch", event => {
-    console.log("fetch event", event)
+self.addEventListener("fetch", e => {
+    console.log("fetch event", e)
+    
+    e.respondWith( //intercept the fetch request and handle it yourself
+        caches.match(e.request) //query the cache for the thing being requested
+        .then(chacheResponse => {
+            console.log("cache response:", chacheResponse)
+            return (chacheResponse || fetch(e.request))  //return cahceReponse, unless its falsy (eg null), then make the fetch request yourself and return that 
+        })
+    )
 })
